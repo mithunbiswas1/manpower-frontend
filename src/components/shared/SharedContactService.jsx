5d35a95e78f2608d0c85456269eb3fe5f50e2e2c
Mithun Biswas <mithunbiswas.me@gmail.com>
2025-08-03 16:30:43 +0600

    updates data dfs

--- src/components/shared/SharedContactService.jsx
+++ src/components/shared/SharedContactService.jsx
@@ -4,13 +4,14 @@ import { PhoneCall, Mail, FileText, Sparkles } from "lucide-react";
 import { SectionSubHeading } from "@/components/custom/SectionSubHeading";
 import { SectionTitle } from "@/components/custom/SectionTitle";
 
-import mission_vision from "@/resource/banner_image_1.jpg";
+import contact_backgeound from "@/resource/contact-backgeound.jpg";
+import Link from "next/link";
 
 export const SharedContactService = () => {
   return (
     <section
       style={{
-        backgroundImage: `url(${mission_vision.src})`,
+        backgroundImage: `url(${contact_backgeound.src})`,
         backgroundSize: "cover",
         backgroundPosition: "center",
         backgroundAttachment: "fixed",
@@ -18,7 +19,11 @@ export const SharedContactService = () => {
     >
       <div className="w-full bg-black/60 text-white">
         <div className="max-w-screen-xl mx-auto py-16 px-4">
-          <motion.div>
+          <motion.div
+            ariants={fadeIn("right", 0.2)}
+            initial="hidden"
+            whileInView="show"
+          >
             <SectionSubHeading
               iconClass="text-white"
               titleClass="text-white"
@@ -47,26 +52,36 @@ export const SharedContactService = () => {
           >
             <div className="flex flex-col items-center">
               <PhoneCall className="w-10 h-10 text-white mb-2" />
-              <p className="text-lg">Reach us</p>
-              <p className="font-semibold text-xl mt-1 tracking-wide">
-                800 82 22 726
-              </p>
+              <p className="text-lg">Call Us</p>
+              <a
+                href="tel:+97143535837"
+                className="font-semibold text-xl mt-1 tracking-wide hover:underline"
+              >
+                04-3535837
+              </a>
             </div>
 
             <div className="flex flex-col items-center">
               <Mail className="w-10 h-10 text-white mb-2" />
-              <p className="text-lg">Quick Mail</p>
-              <p className="font-semibold text-xl mt-1 tracking-wide">
-                info@tabascouae.com
-              </p>
+              <p className="text-lg">Email Us</p>
+              <a
+                href="mailto:info@alsaharagroup.com"
+                className="font-semibold text-xl mt-1 tracking-wide hover:underline"
+              >
+                info@alsaharagroup.com
+              </a>
             </div>
 
             <div className="flex flex-col items-center">
               <FileText className="w-10 h-10 text-white mb-2" />
-              <p className="text-lg">Get Quote</p>
-              <p className="font-semibold text-xl mt-1 tracking-wide">
+              <p className="text-lg">Request a Quote</p>
+              <Link
+                href="/reach-us"
+                rel="noopener noreferrer"
+                className="font-semibold text-xl mt-1 tracking-wide hover:underline"
+              >
                 Book Online
-              </p>
+              </Link>
             </div>
           </motion.div>
         </div>
