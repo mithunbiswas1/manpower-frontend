5a30fd3c82cd9fc11eae54d36f53c94374a9a897
Mithun Biswas <mithunbiswas.me@gmail.com>
2025-08-16 12:30:36 +0600

    updates dsf

--- src/app/career/page.jsx
+++ src/app/career/page.jsx
@@ -7,6 +7,7 @@ import banner_image from "@/resource/banner_image_3.jpg";
 import { SectionTitle } from "@/components/custom/SectionTitle";
 import { SectionSubHeading } from "@/components/custom/SectionSubHeading";
 import { Linkedin, Info } from "lucide-react";
+import { Paragraph } from "@/components/custom/Paragraph";
 
 export default function CareerPage() {
   const [activeTab, setActiveTab] = useState("jobs");
@@ -86,27 +87,30 @@ export default function CareerPage() {
           <SectionTitle className="mb-2 text-center">
             Current Job Openings
           </SectionTitle>
-          <SectionSubHeading className="mb-10 justify-center">
-            Join Our Team
-          </SectionSubHeading>
+          <Paragraph className="text-center max-w-3xl mx-auto">
+            Box Craft Packing Services is a licensed manpower supply company
+            based in the UAE, specializing in providing fully compliant,
+            trained, and reliable labor across packing, construction,
+            hospitality, industrial, and cleaning sectors.
+          </Paragraph>
 
           {/* Tabs */}
-          <div className="flex justify-center mb-12 space-x-2">
+          <div className="flex justify-center my-12 space-x-2">
             <button
-              className={`px-6 py-2 rounded-md font-medium ${
+              className={`px-6 py-2 font-medium ${
                 activeTab === "jobs"
-                  ? "bg-yellow-600 text-white"
-                  : "bg-gray-700 text-white"
+                  ? "bg-secondary text-white"
+                  : "bg-primary text-white"
               }`}
               onClick={() => setActiveTab("jobs")}
             >
               Current opportunities
             </button>
             <button
-              className={`px-6 py-2 rounded-md font-medium ${
+              className={`px-6 py-2 font-medium ${
                 activeTab === "form"
-                  ? "bg-yellow-600 text-white"
-                  : "bg-gray-700 text-white"
+                  ? "bg-secondary text-white"
+                  : "bg-primary text-white"
               }`}
               onClick={() => setActiveTab("form")}
             >
@@ -119,13 +123,13 @@ export default function CareerPage() {
             <div className="space-y-12 text-center">
               {jobs.map((job) => (
                 <div key={job.id}>
-                  <h3 className="text-xl font-semibold mb-3">
+                  <h5 className="_h5 text-gray-500">
                     {job.id}. {job.title}
-                  </h3>
-                  <p className="font-medium mb-2 text-gray-700">
+                  </h5>
+                  <p className="_p mt-2 !text-gray-500 !font-semibold">
                     Requirements:
                   </p>
-                  <ul className="text-gray-600 mb-4">
+                  <ul className="text-gray-600 mb-4 text-xs md:text-sm">
                     {job.requirements.map((req, i) => (
                       <li key={i}>{req}</li>
                     ))}
@@ -133,27 +137,34 @@ export default function CareerPage() {
                   <div className="flex justify-center gap-2">
                     <a
                       href="#"
-                      className="flex items-center gap-2 bg-[#0074e6] text-white px-4 py-2 rounded-sm font-semibold"
+                      className="flex items-center gap-2 bg-primary text-white font-semibold border border-primary"
                     >
                       {/* Indeed Icon */}
-                      <span className="w-5 h-5 flex items-center justify-center">
+                      <span className="w-10 h-10 flex items-center justify-center bg-white">
                         <svg
                           xmlns="http://www.w3.org/2000/svg"
                           viewBox="0 0 24 24"
-                          fill="white"
+                          fill="primary"
                           className="w-4 h-4"
                         >
                           <path d="M10.824 16.516c-1.397.042-2.793-.414-3.832-1.285-.595-.506-1.127-1.11-1.583-1.812-.456-.702-.82-1.493-1.092-2.348-.272-.855-.443-1.765-.512-2.709-.07-.944.004-1.9.22-2.827.217-.927.568-1.802 1.054-2.585.485-.783 1.09-1.468 1.8-2.03.71-.56 1.523-.993 2.408-1.296.885-.303 1.834-.473 2.798-.485 1.488-.016 2.973.35 4.316 1.098 1.344.748 2.502 1.803 3.407 3.093.905 1.29 1.558 2.768 1.91 4.33.353 1.562.402 3.167.147 4.75-.255 1.583-.873 3.102-1.815 4.463-.942 1.36-2.203 2.5-3.69 3.385-1.487.884-3.178 1.455-4.935 1.691-.013.003-.028.003-.042.004h-.01c-1.18.06-2.36-.024-3.52-.258-1.16-.233-2.28-.66-3.32-1.258-.027-.015-.055-.03-.082-.045zm3.17-10.457c-.896-.007-1.78.18-2.6.55s-1.56.88-2.17 1.54c-.61.66-1.06 1.43-1.34 2.27s-.4 1.76-.32 2.65c.08.89.37 1.76.85 2.53.48.77 1.1 1.43 1.83 1.96.73.53 1.55.93 2.42 1.18.88.25 1.8.34 2.73.27.93-.07 1.84-.33 2.7-.79.86-.46 1.63-1.12 2.29-1.95.66-.83 1.2-1.78 1.58-2.8.38-1.02.5-2.11.36-3.19-.14-1.08-.54-2.13-1.18-3.08-.64-.95-1.5-1.78-2.52-2.42-1.02-.64-2.16-1.03-3.35-1.17-.05-.005-.1-.01-.15-.015z" />
                         </svg>
                       </span>
-                      Apply Now on Indeed
+                      <span className="max-w-32 text-sm ">
+                        Apply Now on Indeed
+                      </span>
                     </a>
                     <a
                       href="#"
-                      className="flex items-center gap-2 bg-[#2D5B7B] text-white px-4 py-2 rounded-sm font-semibold"
+                      className="flex items-center gap-2 bg-[#0a66c2] text-white font-semibold border border-[#0a66c2]"
                     >
-                      <Linkedin size={18} />
-                      Apply Now on LinkedIn
+                      <span className="w-10 h-10 flex items-center justify-center bg-white">
+                        <Linkedin className="text-[#0a66c2]" size={18} />
+                      </span>
+
+                      <span className="max-w-32 text-sm ">
+                        Apply Now on LinkedIn
+                      </span>
                     </a>
                   </div>
                 </div>
