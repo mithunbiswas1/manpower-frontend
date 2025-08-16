d39825c0349168034dcd4d4d6bb5e2ca155bbfab
Mithun Biswas <mithunbiswas.me@gmail.com>
2025-08-16 13:09:56 +0600

    updates f

--- src/components/shared/SharedContact.jsx
+++ src/components/shared/SharedContact.jsx
@@ -2,6 +2,8 @@
 
 import { Linkedin, Instagram, Facebook, Twitter, Youtube } from "lucide-react";
 import { Formik, Form, Field } from "formik";
+import location from "@/resource/location.svg";
+import Image from "next/image";
 
 export const SharedContact = () => {
   return (
@@ -11,7 +13,7 @@ export const SharedContact = () => {
       </h2>
       <p className="text-gray-700 mb-2">Thank you for visiting our page!</p>
       <p className="text-gray-700 mb-2">
-        Dial <span className="font-semibold">800-TABASCO</span> or send us an
+        Dial <span className="font-semibold">Box Craft</span> or send us an
         email now! <br />
         Our team will be glad to assist you.
       </p>
@@ -139,15 +141,9 @@ export const SharedContact = () => {
           8ᵗʰ Floor, Damas Tower, Sharjah, United Arab Emirates.
         </p>
         <div className="mt-6">
-          {/* Replace this with an actual map icon from lucid-react or a custom SVG */}
-          <svg
-            className="h-12 w-12 text-gray-700"
-            fill="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5S10.62 6.5 12 6.5s2.5 1.12 2.5 2.5S13.38 11.5 12 11.5z" />
-          </svg>
+          <a href="#" target="_blank">
+            <Image src={location} alt="map" />
+          </a>
         </div>
       </div>
     </div>
